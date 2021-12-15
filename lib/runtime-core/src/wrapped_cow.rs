d1ee9e7881fc8218c1f0b4baf3469e17e85220ba
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-12-15 22:29:06 +0200

    More type matching for WrappedCow

--- lib/runtime-core/src/wrapped_cow.rs
+++ lib/runtime-core/src/wrapped_cow.rs
@@ -2,13 +2,39 @@
 //! the rkyv::Archive trait.
 
 use std::borrow::Cow;
+use core::borrow::Borrow;
 use core::ops::Deref;
+use std::hash::Hash;
+use std::cmp::PartialEq;
 
 use rkyv::{Archive, Serialize as RkyvSerialize, Deserialize as RkyvDeserialize};
 
 /// A newtype that wraps borrow::Cow.
-#[derive(Archive, RkyvSerialize, RkyvDeserialize)]
-pub struct WrappedCow<'a, B: ?Sized + ToOwned + 'a>(Cow<'a, B>);
+#[derive(Hash, Archive, RkyvSerialize, RkyvDeserialize, Serialize, Deserialize)]
+pub struct WrappedCow<'a, B: ?Sized + ToOwned +  'a>(Cow<'a, B>);
+
+impl<'a, B: ?Sized> Borrow<B> for WrappedCow<'a, B>
+where
+    B: ToOwned + Clone,
+    <B as ToOwned>::Owned: 'a,
+{
+    fn borrow(&self) -> &B {
+        self.0.borrow()
+    }
+}
+
+impl<B: ?Sized + Clone> Eq for WrappedCow<'_, B> where B: Eq + ToOwned {}
+
+impl<'a, 'b, B: ?Sized, C: ?Sized> PartialEq<WrappedCow<'b, C>> for WrappedCow<'a, B>
+where
+    B: PartialEq<C> + ToOwned + Clone,
+    C: ToOwned + Clone,
+{
+    #[inline]
+    fn eq(&self, other: &WrappedCow<'b, C>) -> bool {
+        self.0.eq(&other.0)
+    }
+}
 
 impl<B: ?Sized + ToOwned + Clone> Clone for WrappedCow<'_, B> {
     /// Passthrough method.
@@ -43,16 +69,33 @@ impl<B: ?Sized + ToOwned + Clone> Deref for WrappedCow<'_, B> {
     }
 }
 
-impl<'a, T: Clone> From<Vec<T>> for Cow<'a, [T]> {
-    /// Newtype requirement.
-    fn from(v: Vec<T>) -> Cow<'a, [T]> {
-        Cow::Owned(v)
+impl<B: ?Sized + ToOwned + Clone> AsRef<B> for WrappedCow<'_, B> {
+    /// Passthrough method.
+    fn as_ref(&self) -> &B {
+        self.0.as_ref()
     }
 }
 
-impl<'a, T: Clone> From<&'a Vec<T>> for Cow<'a, [T]> {
+impl<'a, B: Clone> From<Vec<B>> for WrappedCow<'a, [B]> {
     /// Newtype requirement.
-    fn from(v: &'a Vec<T>) -> Cow<'a, [T]> {
-        Cow::Borrowed(v.as_slice())
+    fn from(v: Vec<B>) -> WrappedCow<'a, [B]> {
+        WrappedCow(Cow::Owned(v))
+    }
+}
+
+impl<'a, B: Clone> From<&'a Vec<B>> for WrappedCow<'a, [B]> {
+    /// Newtype requirement.
+    fn from(v: &'a Vec<B>) -> WrappedCow<'a, [B]> {
+        WrappedCow(Cow::Borrowed(v.as_slice()))
+    }
+}
+
+impl<'a, B> From<&'a [B]> for WrappedCow<'a, [B]>
+where
+    [B]: ToOwned
+{
+    /// Newtype requirement
+    fn from(s: &'a [B]) -> WrappedCow<'a, [B]> {
+        WrappedCow(Cow::Borrowed(s))
     }
 }
