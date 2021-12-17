27de7f36dd8105cd234a11d34597528c308e8344
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-12-17 12:20:11 +0200

    More attempts

--- lib/runtime-core/src/wrapped_cow.rs
+++ lib/runtime-core/src/wrapped_cow.rs
@@ -6,21 +6,65 @@ use core::borrow::Borrow;
 use core::ops::Deref;
 use std::hash::Hash;
 use std::cmp::PartialEq;
-use serde;
+use serde::{Serialize, Deserialize};
+use crate::types;
 
 use rkyv::{Archive, Serialize as RkyvSerialize, Deserialize as RkyvDeserialize};
 
 /// A newtype that wraps borrow::Cow.
-#[derive(PartialEq, Debug, Hash, Archive, RkyvSerialize, RkyvDeserialize, Serialize, Deserialize)]
-pub struct WrappedCow<'a, B: ?Sized + ToOwned + serde::Serialize + serde::Deserialize<'a> + 'a>(pub Cow<'a, B>);
+#[derive(PartialEq, Debug, Hash, Archive, RkyvSerialize, RkyvDeserialize)]
+pub struct WrappedCow<'a, B: ?Sized + ToOwned + 'a>(pub Cow<'a, B>);
 
-impl<'a, B: ?Sized + serde::Serialize + serde::Deserialize<'a>> Borrow<B> for WrappedCow<'a, B>
+impl<'a, B: ?Sized> Borrow<B> for WrappedCow<'a, B>
 where
     B: ToOwned + Clone,
     <B as ToOwned>::Owned: 'a,
 {
     fn borrow(&self) -> &B {
         self.0.borrow()
+    }
+}
+
+impl <'a, B: ?Sized + Clone> serde::Serialize for WrappedCow<'a, B> {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        (**self).serialize(serializer)
+    }
+}
+
+// impl<'de, 'a, B: ?Sized + ToOwned + serde::Serialize + serde::Deserialize<'de>> serde::Deserialize<'de> for WrappedCow<'a, B>
+// where
+//     B: ToOwned,
+//     B::Owned: serde::Deserialize<'de>,
+//     'a: 'de,
+// {
+//     #[inline]
+//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+//     where
+//         D: serde::Deserializer<'de>,
+//     {
+//         match B::Owned::deserialize(deserializer) {
+//             Ok(b) => WrappedCow(Cow::Owned(b)),
+//             Err(e) => Err(e)
+//         }
+//     }
+// }
+
+impl serde::Deserialize<'static> for WrappedCow<'static, [types::Type]>
+where
+{
+    #[inline]
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'static>,
+    {
+        match Vec::<[types::Type]>::deserialize(deserializer) {
+            Ok(b) => WrappedCow(Cow::Owned(b)),
+            Err(e) => Err(e)
+        }
     }
 }
 
@@ -37,7 +81,7 @@ where
 //     }
 // }
 
-impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>> Clone for WrappedCow<'a, B> {
+impl<'a, B: ?Sized + ToOwned + Clone> Clone for WrappedCow<'a, B> {
     /// Passthrough method.
     fn clone(&self) -> Self {
         WrappedCow(self.0.clone())
@@ -49,7 +93,7 @@ impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>
     }
 }
 
-impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>> WrappedCow<'a, B> {
+impl<'a, B: ?Sized + ToOwned + Clone> WrappedCow<'a, B> {
     /// Passthrough method.
     pub fn to_mut(&mut self) -> &mut <B as ToOwned>::Owned {
         self.0.to_mut()
@@ -61,7 +105,7 @@ impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>
     }
 }
 
-impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>> Deref for WrappedCow<'a, B> {
+impl<'a, B: ?Sized + ToOwned + Clone> Deref for WrappedCow<'a, B> {
     type Target = B;
 
     /// Passthrough method.
@@ -70,28 +114,34 @@ impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>
     }
 }
 
-impl<'a, B: ?Sized + ToOwned + Clone + serde::Serialize + serde::Deserialize<'a>> AsRef<B> for WrappedCow<'a, B> {
+impl<'a, B: ?Sized + ToOwned + Clone> AsRef<B> for WrappedCow<'a, B> {
     /// Passthrough method.
     fn as_ref(&self) -> &B {
         self.0.as_ref()
     }
 }
 
-impl<'a, B: Clone + serde::Serialize + serde::Deserialize<'a>> From<Vec<B>> for WrappedCow<'a, [B]> {
+impl<'a, B: Clone> From<Vec<B>> for WrappedCow<'a, [B]>
+where
+    [B]: ToOwned
+{
     /// Newtype requirement.
     fn from(v: Vec<B>) -> WrappedCow<'a, [B]> {
         WrappedCow(Cow::Owned(v))
     }
 }
 
-impl<'a, B: Clone + serde::Serialize + serde::Deserialize<'a>> From<&'a Vec<B>> for WrappedCow<'a, [B]> {
+impl<'a, B: Clone> From<&'a Vec<B>> for WrappedCow<'a, [B]>
+where
+    [B]: ToOwned
+{
     /// Newtype requirement.
     fn from(v: &'a Vec<B>) -> WrappedCow<'a, [B]> {
         WrappedCow(Cow::Borrowed(v.as_slice()))
     }
 }
 
-impl<'a, B: serde::Serialize + serde::Deserialize<'a>> From<&'a [B]> for WrappedCow<'a, [B]>
+impl<'a, B> From<&'a [B]> for WrappedCow<'a, [B]>
 where
     [B]: ToOwned
 {
