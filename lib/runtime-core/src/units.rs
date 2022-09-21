134c0b337e89ff9d8ac22ee6a1383b4fe5df78aa
Ovidiu Stinga <ovstinga@gmail.com>
2022-09-21 20:15:59 +0300

    fix warnings for release build

--- lib/runtime-core/src/units.rs
+++ lib/runtime-core/src/units.rs
@@ -18,6 +18,7 @@ pub const WASM_MIN_PAGES: usize = 256;
 
 /// Units of WebAssembly pages (as specified to be 65,536 bytes).
 #[derive(Serialize, Deserialize, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Archive, RkyvSerialize, RkyvDeserialize)]
+#[repr(C)]
 pub struct Pages(pub u32);
 
 impl Pages {
