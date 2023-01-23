f2a58d838646e07e05403ac6339255ac618d2b4c
Ovidiu Stinga <ovstinga@gmail.com>
2023-01-23 17:29:50 +0200

    small refactor

--- lib/middleware-common/src/opcode_trace.rs
+++ lib/middleware-common/src/opcode_trace.rs
@@ -1,5 +1,4 @@
 use std::fs::File;
-use std::io;
 use std::io::Write;
 
 use wasmer_runtime_core::{
@@ -27,46 +26,6 @@ impl OpcodeTracer {
         }
     }
 
-    pub fn trace_instance_exports(&mut self, instance: &Instance) -> io::Result<()> {
-        write!(self.output_file, "{:#?}\n", instance.module.info.exports)
-    }
-
-    pub fn trace_event(&mut self, ev: &Event, source_loc: u32) -> io::Result<()> {
-        match *ev {
-            Event::Internal(InternalEvent::FunctionBegin(function_index)) => {
-                self.trace_function_begin(function_index)
-            }
-            Event::Internal(InternalEvent::FunctionEnd) => self.trace_function_end(),
-            _ => self.trace_opcode_event(ev, source_loc),
-        }
-    }
-
-    pub fn trace_function_begin(&mut self, function_index: u32) -> io::Result<()> {
-        write!(self.output_file, "FUNCTION BEGIN: {}\n", function_index)
-    }
-
-    pub fn trace_function_end(&mut self) -> io::Result<()> {
-        write!(self.output_file, "FUNCTION END\n")
-    }
-
-    pub fn trace_opcode_event(&mut self, ev: &Event, source_loc: u32) -> io::Result<()> {
-        match ev {
-            Event::Wasm(&ref op) | Event::WasmOwned(ref op) => {
-                write!(self.output_file, "\t{}:\t{:?}\n", source_loc, *op)
-            }
-            _ => Ok(()),
-        }
-    }
-
-    pub fn push_last_location_tracer(&self, sink: &mut EventSink, source_loc: u32) {
-        sink.push(Event::WasmOwned(Operator::I64Const {
-            value: source_loc as i64,
-        }));
-        sink.push(Event::Internal(InternalEvent::SetInternal(
-            OPCODE_LAST_LOCATION.index() as _,
-        )));
-    }
-
     fn trace_operator(&mut self, event: &Event) {
         match *event {
             Event::Internal(InternalEvent::FunctionBegin(local_function_index)) => {
@@ -92,6 +51,15 @@ impl OpcodeTracer {
             _ => {}
         }
     }
+
+    fn push_last_location_tracer(&self, sink: &mut EventSink, source_loc: u32) {
+        sink.push(Event::WasmOwned(Operator::I64Const {
+            value: source_loc as i64,
+        }));
+        sink.push(Event::Internal(InternalEvent::SetInternal(
+            OPCODE_LAST_LOCATION.index() as _,
+        )));
+    }
 }
 
 impl FunctionMiddleware for OpcodeTracer {
@@ -101,12 +69,10 @@ impl FunctionMiddleware for OpcodeTracer {
         op: Event<'a, 'b>,
         _module_info: &ModuleInfo,
         sink: &mut EventSink<'a, 'b>,
-        _source_loc: u32,
+        source_loc: u32,
     ) -> Result<(), Self::Error> {
-        // self.trace_event(&op, source_loc)
-        //     .expect("failed to trace event");
-        // self.push_last_location_tracer(sink, source_loc);
         self.trace_operator(&op);
+        self.push_last_location_tracer(sink, source_loc);
 
         sink.push(op);
 
