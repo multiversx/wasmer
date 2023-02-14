b596e20606d28a05ad92ef2762cd9abd56d90446
Ovidiu Stinga <ovstinga@gmail.com>
2023-02-14 13:38:59 +0200

    add some benchmark logging & add fast exit for shrink memory

--- lib/vm/src/instance/reset.rs
+++ lib/vm/src/instance/reset.rs
@@ -5,31 +5,42 @@ use wasmer_types::{DataInitializer, OwnedDataInitializer};
 impl InstanceHandle {
     /// Resets the `[Globals`] and [`Memories`] for an [`Instance`].
     pub fn reset(&self, data_initializers: &[OwnedDataInitializer]) -> Result<(), String> {
+        let now = std::time::Instant::now();
         let instance = self.instance.as_ref();
         reset_passive_elements(instance);
         reset_globals(instance);
-        reset_memories(instance, data_initializers)
+        reset_memories(instance, data_initializers)?;
+        println!("reset: {:?}", now.elapsed());
+        Ok(())
     }
 }
 
 fn reset_passive_elements(instance: &Instance) {
+    let now = std::time::Instant::now();
     initialize_passive_elements(instance);
+    println!("reset_passive_elements: {:?}", now.elapsed());
 }
 
 fn reset_globals(instance: &Instance) {
+    let now = std::time::Instant::now();
     initialize_globals(instance);
+    println!("reset_globals: {:?}", now.elapsed());
 }
 
 fn reset_memories(
     instance: &Instance,
     data_initializers: &[OwnedDataInitializer],
 ) -> Result<(), String> {
+    let now = std::time::Instant::now();
     zero_memories(instance)?;
     shrink_memories(instance)?;
-    reinitialize_memories(instance, data_initializers)
+    reinitialize_memories(instance, data_initializers)?;
+    println!("reset_memories: {:?}", now.elapsed());
+    Ok(())
 }
 
 fn zero_memories(instance: &Instance) -> Result<(), String> {
+    let now = std::time::Instant::now();
     for (_local_memory_index, memory) in instance.memories.iter() {
         unsafe {
             let memory = memory.vmmemory().as_ref();
@@ -46,12 +57,14 @@ fn zero_memories(instance: &Instance) -> Result<(), String> {
             }
         }
     }
-
+    println!("zero_memories: {:?}", now.elapsed());
     Ok(())
 }
 
 fn shrink_memories(instance: &Instance) -> Result<(), String> {
+    let now = std::time::Instant::now();
     for (_local_memory_index, memory) in instance.memories.iter() {
+        println!("memory size before shrink: {:?}", memory.size());
         let result = memory.shrink_to_minimum();
         if let Err(memory_error) = result {
             match memory_error {
@@ -60,8 +73,9 @@ fn shrink_memories(instance: &Instance) -> Result<(), String> {
                 _ => return Err(String::from("unexpected memory error")),
             }
         }
+        println!("memory size after shrink: {:?}", memory.size());
     }
-
+    println!("shrink_memories: {:?}", now.elapsed());
     Ok(())
 }
 
@@ -69,6 +83,7 @@ fn reinitialize_memories(
     instance: &Instance,
     data_initializers: &[OwnedDataInitializer],
 ) -> Result<(), String> {
+    let now = std::time::Instant::now();
     let data_initializers = data_initializers
         .iter()
         .map(|init| DataInitializer {
@@ -86,6 +101,6 @@ fn reinitialize_memories(
             _ => return Err(String::from("unexpected trap")),
         }
     }
-
+    println!("reinitialize_memories: {:?}", now.elapsed());
     Ok(())
 }
